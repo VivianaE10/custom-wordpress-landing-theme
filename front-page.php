1f41f63df2d7fb0aa3ac128c1d375eab4ba34823
Viviana Ospina <vyvy_escobar@hotmail.com>
2026-05-06 16:29:24 -0500

    Estructura base del landing page hero

---
+++ front-page.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+
+<div class="contenedor-principal">
+
+<section class="hero">
+
+    <!-- OVERLAY -->
+    <div class="overlay-hero"></div>
+
+   
+    <img
+        class="nube-izquierda"
+        src="<?php echo get_template_directory_uri(); ?>/assets/img/nube-izquierda.png"
+        alt="nube izquierda"
+    >
+
+ 
+    <img
+        class="nube-derecha"
+        src="<?php echo get_template_directory_uri(); ?>/assets/img/nube-derecha.png"
+        alt="nube derecha"
+    >
+
+    <!-- CONTENIDO HERO -->
+    <div class="contenido-hero">
+
+        <!-- TAKE OFF -->
+        <img
+            class="imagen-takeoff"
+            src="<?php echo get_template_directory_uri(); ?>/assets/img/take-off.png"
+            alt="take off"
+        >
+
+        <!-- DESCRIPCIÓN -->
+        <!-- <p class="descripcion-hero">
+            Avoid turbulence. Arrive at your destination sooner. And enjoy the ascent
+        </p>
+
+      
+        <img
+            class="boton"
+            src="<?php echo get_template_directory_uri(); ?>/assets/img/boton1.png"
+            alt="boton"
+        > -->
+
+    </div>
+
+</section>
+
+</div>
+
+<?php get_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function cargar_estilos_theme() {
+
+    wp_enqueue_style(
+        'estilos-theme',
+        get_stylesheet_uri()
+    );
+
+}
+
+add_action('wp_enqueue_scripts', 'cargar_estilos_theme');
