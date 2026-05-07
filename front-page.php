92f9a09eb49a49d56f14d5510d779ce7e1b1ad41
Viviana Ospina <vyvy_escobar@hotmail.com>
2026-05-06 19:02:57 -0500

    Maquetación completa de la sección B2B con integración visual al hero y optimización del background global

--- front-page.php
+++ front-page.php
@@ -30,20 +30,72 @@
             src="<?php echo get_template_directory_uri(); ?>/assets/img/take-off.png"
             alt="take off"
         >
+    </div>
 
-        <!-- DESCRIPCIÓN -->
-        <!-- <p class="descripcion-hero">
+</section>
+
+<!-- -------SECCIÓN 2 -->
+ <section class="seccion-b2b">
+
+    <!-- PARTE SUPERIOR CENTRADA -->
+    <div class="intro-b2b">
+
+        <!-- DESCRIPCIÓN HERO -->
+        <p class="descripcion-hero">
             Avoid turbulence. Arrive at your destination sooner. And enjoy the ascent
         </p>
 
-      
+        <!-- BOTÓN -->
         <img
             class="boton"
             src="<?php echo get_template_directory_uri(); ?>/assets/img/boton1.png"
             alt="boton"
-        > -->
+        >
 
     </div>
+
+
+    <!-- CONTENIDO IZQUIERDA -->
+    <div class="contenido-b2b">
+
+        <!-- TITULO -->
+        <h2 class="titulo-b2b">
+            B2B SUCCESS IS IN THE AIR
+        </h2>
+
+        <!-- TEXTO -->
+        <p class="texto-b2b">
+            JumpSeat exists for the sole purpose to help you create momentum.
+        </p>
+
+        <!-- TEXTO -->
+        <p class="texto-b2b">
+            Momentum is the unmistakable energy on a winning B2B revenue team. It gives everyone the confidence to push themselves further, faster and to greater heights.
+        </p>
+
+        <!-- BOTÓN -->
+        <img
+            class="boton-b2b"
+            src="<?php echo get_template_directory_uri(); ?>/assets/img/boton1.png"
+            alt="boton"
+        >
+
+    </div>
+
+
+    <!-- AIRPLANE -->
+    <div class="airplane-section">
+
+        <img
+            class="airplane-board"
+            src="<?php echo get_template_directory_uri(); ?>/assets/img/airplane.png"
+            alt="airplane board"
+        >
+
+    </div>
+
+</section>
+  
 
 </section>
 
