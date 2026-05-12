426008f91a134f740c114ff75cb1adf5619e1116
Viviana Ospina <vyvy_escobar@hotmail.com>
2026-05-12 18:07:51 -0500

    (footer): implementar estructura del footer con logo, informacion de contacto, icono de LinkedIn y estilos personalizados basados en el diseno

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+  <div class="footer__contenedor">
+
+    <div class="footer__logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Logo">
+    </div>
+
+    <div class="footer__icono">
+      <img class="footer__circulo" src="<?php echo get_template_directory_uri(); ?>/assets/img/Ellipse.png" alt="">
+      <img class="footer__linkedin" src="<?php echo get_template_directory_uri(); ?>/assets/img/linkedin.png" alt="LinkedIn">
+    </div>
+
+    <div class="footer__contacto">
+      <h3>CONTACT US</h3>
+      <p>3116 W Cortland<br>Chicago, IL 60647</p>
+      <p>630.870.2141<br>letsfly@web.com</p>
+    </div>
+
+    <div class="footer__links">
+      <a href="#">OUR SERVICES</a>
+      <a href="#">BELIEFS</a>
+      <a href="#">ABOUT US</a>
+      <a href="#">JOIN US</a>
+    </div>
+
+  </div>
+</footer>
